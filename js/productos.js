1bfde6f6970cdad30a195794e191ed7aaf0616d7
Nataly <natalymt03@gmail.com>
2023-02-20 20:53:29 +0100

    responsive productos

--- js/productos.js
+++ js/productos.js
@@ -1,24 +1,24 @@
 let contenedorProductos = document.getElementById("productos");
         let producto = [
-            ['./upload/productos/1.jpg','muletras_para_perro','Muletas para perro',3.4],
-            ['./upload/productos/2.jpg','muletras_para_perro','Muletas para perro',3.5],
-            ['./upload/productos/3.jpg','muletras_para_perro','Muletas para perro',3.6],
-            ['./upload/productos/4.jpg','muletras_para_perro','Muletas para perro',3.7],
-            ['./upload/productos/5.jpg','muletras_para_perro','Muletas para perro',3.8],
-            ['./upload/productos/6.jpg','muletras_para_perro','Muletas para perro',3.9],
-            ['./upload/productos/7.jpg','muletras_para_perro','Muletas para perro',3.4],
-            ['./upload/productos/8.jpg','muletras_para_perro','Muletas para perro',3.5],
-            ['./upload/productos/9.jpg','muletras_para_perro','Muletas para perro',3.6],
-            ['./upload/productos/10.jpg','muletras_para_perro','Muletas para perro',3.6],
-            ['./upload/productos/11.jpg','muletras_para_perro','Muletas para perro',3.6],
-            ['./upload/productos/12.jpg','muletras_para_perro','Muletas para perro',3.6],
-            ['./upload/productos/13.jpg','muletras_para_perro','Muletas para perro',3.6],
-            ['./upload/productos/14.jpg','muletras_para_perro','Muletas para perro',3.6],
-            ['./upload/productos/15.jpg','muletras_para_perro','Muletas para perro',3.6],
-            ['./upload/productos/16.jpg','muletras_para_perro','Muletas para perro',3.6],
-            ['./upload/productos/17.jpg','muletras_para_perro','Muletas para perro',3.6],
-            ['./upload/productos/18.jpg','muletras_para_perro','Muletas para perro',3.6],
-            ['./upload/productos/19.jpg','muletras_para_perro','Muletas para perro',3.6],
+            ['./upload/productos/1.jpg','chuche_perro','Comida liquida para perro',3.4],
+            ['./upload/productos/2.jpg','','Plato de comida para perro',3.5],
+            ['./upload/productos/3.jpg','','Plato de comida para perro nervioso',3.6],
+            ['./upload/productos/4.jpg','','Cepillo para perro',3.7],
+            ['./upload/productos/5.jpg','','Silla de ruedas para perro',3.8],
+            ['./upload/productos/6.jpg','','Jarabe para para perro',3.9],
+            ['./upload/productos/7.jpg','','Dulces de cacahuete para perro',3.4],
+            ['./upload/productos/8.jpg','','Barritas dentales para perro',3.5],
+            ['./upload/productos/9.jpg','','Limpiador de oidos para perro',3.6],
+            ['./upload/productos/10.jpg','','Limpiador de oidos para perro',3.6],
+            ['./upload/productos/11.jpg','','Cepillos dentales + pasta dental  para perro',3.6],
+            ['./upload/productos/12.jpg','','Bolsitas para perro',3.6],
+            ['./upload/productos/13.jpg','','Pañales para perro',3.6],
+            ['./upload/productos/14.jpg','','Suplementos para perros mayores',3.6],
+            ['./upload/productos/15.jpg','','Chaqueta para perro',3.6],
+            ['./upload/productos/16.jpg','','Chaqueta para perro',3.6],
+            ['./upload/productos/17.jpg','','Bolsa transporte para perro',3.6],
+            ['./upload/productos/18.jpg','','Funda de coche para perro',3.6],
+            ['./upload/productos/19.jpg','','Collar para perro',3.6],
         ]
 
         let contenido;
@@ -36,4 +36,5 @@ let contenedorProductos = document.getElementById("productos");
             }
             contenedorProductos.innerHTML+=contenido;
         }
+        contenedorProductos.innerHTML+="<br>";
         
